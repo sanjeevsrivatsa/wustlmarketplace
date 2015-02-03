2799be958f82d2b231f08f137689474b4e366a8b
Andrew Buckley <a@bckly.me>
2015-02-03 17:51:04 -0600

    new form and registration
    modified from bootsnipp

--- src/users.js
+++ src/users.js
@@ -10,7 +10,7 @@ users.controller('UserSignupCtrl', ['$scope', '$http',
     $scope.signup = function(user) {
         $scope.master = angular.copy(user);
         $scope.master.toString();
-        $http.post(API_ROOT+USERS_ROOT, {name:$scope.master.name, username:$scope.master.email, email:$scope.master.email, password:$scope.master.password}).
+        $http.post(API_ROOT+USERS_ROOT, {first_name:$scope.master.first_name, last_name:$scope.master.last_name, display_name:$scope.master.display_name, username:$scope.master.email, email:$scope.master.email, password:$scope.master.password}).
           success(function(data, status, headers, config){
             //alert(JSON.stringify(data));
           }).
