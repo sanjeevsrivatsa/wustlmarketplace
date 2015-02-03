d27083c711b3c730da3b5906866c0587c6f317f2
Andrew Buckley <a@bckly.me>
2015-02-03 00:17:05 -0600

    take 2

--- src/users.js
+++ src/users.js
@@ -9,7 +9,7 @@ users.controller('UserSignupCtrl', ['$scope', '$http',
     $scope.master = {};
     $scope.signup = function(user) {
         $scope.master = angular.copy(user);
-        $http.post(API_ROOT+USERS_ROOT, {"name":master.name, "email":master.email, "password":master.password}).
+        $http.post(API_ROOT+USERS_ROOT, {"name":$scope.master.name, "email":$scope.master.email, "password":$scope.master.password}).
           success(function(data, status, headers, config){
             alert("SWEET");
           }),
